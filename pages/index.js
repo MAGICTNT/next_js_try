704bcd0efaf903da921d90707effe332c443fdc4
Max <maxjavaux@gmail.com>
2021-10-13 20:01:55 +0200

    update 13oct

--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,34 @@
-/* eslint-disable react/jsx-key */
-
 import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
-import { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
 
-export default function Home({ posts, date }) {
-  const [count, setCount] = useState(0)
-
-  useEffect(() => {
-    const timer = setInterval(() => setCount(n => n + 1), 1000)
-    return () => {
-      clearInterval(timer)
-    }
-}, [])
-
+export default function Home() {
   return (
-    <>
-      <Head>
-        <title>test site</title>
-      </Head>
-      <h1>date : { date }</h1>
-      <h1>count : { count }</h1>
-      <ul>
-        {posts.map(post => <li>
-          <Link href={`/blog/${post.id}`}>
-          <a>
-          <h3>{post.id} - { post.title } </h3>
-          </a>
-          </Link>
-        </li>)}
-      </ul>
-    </>
-  )
-}
+      <><Head>
+      <title>Blog next.js</title>
+    </Head>
+      <body className={styles.section}>
+        <section className={styles.container}>
+          <div>
+            <h1>
+              Bonjour je suis Maxime Javaux et ceci est mon premier test avec :
+            </h1>
+            <Link href="https://nextjs.org">
+              <a className={styles.title}>next.js</a>
+            </Link>
+          </div>
 
-export async function getStaticProps() {
-      const posts =await fetch("https://jsonplaceholder.typicode.com/posts?_limit=4")
-        .then(r => r.json())
-  return {   // je retourne un objet qui contien props et dans c'est props on renvoi les articles
-    props: {
-      posts,
-      date: (new Date()).toString()
-    },
-    revalidate: 5,
-  }
+          <div>
+            <p className={styles.pHome}>
+              Dans le futur cette page deviendra un blogue pour me faire la main
+              sur ce framwork mais vous trouverai quelque petit projet sur mon
+            </p>
+            <Link href="https://github.com/MAGICTNT">
+              <a className={styles.lien}>gitHub</a>
+            </Link>
+          </div>
+        </section>
+      </body></>
+  );
 }
-
